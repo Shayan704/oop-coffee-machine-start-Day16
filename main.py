79a09fff1c63ed1b4193f92758206feb5d625ef9
Shayan <shayanmandal100@gmail.com>
2023-08-15 10:19:47 +0530

    final touches

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
-
+from art import art
 menu = Menu()
 
 # menuitem1 = MenuItem()
@@ -13,10 +12,10 @@ money_machine = MoneyMachine()
 
 
 
-
 isTrue = True
 
 while isTrue:
+    print(art)
     choice=input(f"What would you like?{menu.get_items()}\t")
     if choice == "report":
         coffee_maker.report()
@@ -28,4 +27,3 @@ while isTrue:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
-
